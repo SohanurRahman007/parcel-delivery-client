d31e964f84671d45c4ac2528707cf038f933a89b
Sohanur Rahman <sohanuractive007@gmail.com>
2025-06-30 15:08:44 +0600

    jwt setup

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,58 +1,142 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import useAuth from '../../../hooks/useAuth';
-import { Link } from 'react-router';
-import SocialLogin from '../SocialLogin/SocialLogin';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import axios from "axios";
+import userAxios from "../../../hooks/userAxios";
 
 const Register = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { createUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { createUser, updateUserProfile } = useAuth();
+  const [profilePic, setProfilePic] = useState();
+  const axiosInstance = userAxios();
 
-    const onSubmit = data => {
-        console.log(data);
-        createUser(data.email, data.password)
-            .then(result => {
-                console.log(result.user)
-            })
-            .catch(error => {
-                console.error(error);
-            })
-    }
+  const onSubmit = (data) => {
+    createUser(data.email, data.password)
+      .then(async (result) => {
+        console.log(result.user);
 
-    return (
+        //   update info in the database
+        const userIfo = {
+          email: data.email,
+          role: "user", //default
+          create_at: new Date().toISOString(),
+          last_log_in: new Date().toISOString(),
+        };
 
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-                <h1 className="text-5xl font-bold">Create Account</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <fieldset className="fieldset">
-                        {/* email field */}
-                        <label className="label">Email</label>
-                        <input type="email"
-                            {...register('email', { required: true })}
-                            className="input" placeholder="Email" />
-                        {
-                            errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>
-                        }
+        const userRes = await axiosInstance.post("/users", userIfo);
+        console.log(userRes.data);
 
-                        <label className="label">Password</label>
-                        <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
-                        {
-                            errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>
-                        }
-                        {
-                            errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be 6 characters or longer</p>
-                        }
+        //   update profile image firebase
+        const userProfile = {
+          displayName: data.name,
+          photoURL: profilePic,
+        };
+        updateUserProfile(userProfile)
+          .then(() => {
+            console.log("name, photo update");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-                        <div><a className="link link-hover">Forgot password?</a></div>
-                        <button className="btn btn-primary text-black mt-4">Register</button>
-                    </fieldset>
-                    <p><small>Already have an account? <Link className="btn btn-link" to="/login">Login</Link></small></p>
-                </form>
-                <SocialLogin></SocialLogin>
+  const handleUploadImage = async (e) => {
+    const image = e.target.files[0];
+    console.log(image);
+    const formData = new FormData();
+    formData.append("image", image);
+    const imageUploadUrl = `https://api.imgbb.com/1/upload?&key=${
+      import.meta.env.VITE_upload_key
+    }`;
+    const res = await axios.post(imageUploadUrl, formData);
+
+    setProfilePic(res.data.data.url);
+  };
+
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-5xl font-bold">Create Account</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset">
+            {/* name field */}
+            <label className="label">Name</label>
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              className="input"
+              placeholder="Your Name"
+            />
+            {errors.email?.type === "required" && (
+              <p className="text-red-500">Name is required</p>
+            )}
+            {/* image field */}
+            <label className="label">Image </label>
+            <input
+              onChange={handleUploadImage}
+              type="file"
+              className="input"
+              placeholder="Your Name image"
+            />
+
+            {/* email field */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              className="input"
+              placeholder="Email"
+            />
+            {errors.email?.type === "required" && (
+              <p className="text-red-500">Email is required</p>
+            )}
+            {/* password */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              {...register("password", { required: true, minLength: 6 })}
+              className="input"
+              placeholder="Password"
+            />
+            {errors.password?.type === "required" && (
+              <p className="text-red-500">Password is required</p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p className="text-red-500">
+                Password must be 6 characters or longer
+              </p>
+            )}
+
+            <div>
+              <a className="link link-hover">Forgot password?</a>
             </div>
-        </div>
-    );
+            <button className="btn btn-primary text-black mt-4">
+              Register
+            </button>
+          </fieldset>
+          <p>
+            <small>
+              Already have an account?{" "}
+              <Link className="btn btn-link" to="/login">
+                Login
+              </Link>
+            </small>
+          </p>
+        </form>
+        <SocialLogin></SocialLogin>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
